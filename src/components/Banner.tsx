7ecc5248d121fb37a7ebefaccfcf796371e05f90
Okakakai <inoue.kai.work@gmail.com>
2022-08-28 16:41:16 +0900

    ALL

---
+++ src/components/Banner.tsx
@@ -0,0 +1,66 @@
+import axios from "../axios";
+import { useState, useEffect } from "react";
+import { requests } from "../request";
+
+import "./css/Banner.scss";
+
+type movieProps = {
+  title?: string;
+  name?: string;
+  orignal_name?: string;
+  backdrop_path?: string;
+  overview?: string;
+};
+
+export const Banner = () => {
+  const [movie, setMovie] = useState<movieProps>({});
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.feachNetflixOriginals);
+      console.log(request.data.result);
+
+      //apiからランダムで値を取得する
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
+  //description の切り捨てるための関数
+  function truncate(str: any, n: number) {
+    if (str !== undefined) {
+      //n文字に説明文章を絞るための関数
+      return str.length > n ? str?.substr(0, n - 1) + "..." : str;
+    }
+  }
+
+  return (
+    <header
+      className="Banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="Banner-contents">
+        <h1 className="Banner-title">
+          {movie?.title || movie?.name || movie?.orignal_name}
+        </h1>
+        <div className="Banner-buttons">
+          <button className="Banner-button">Play</button>
+          <button className="Banner-button">My List</button>
+        </div>
+
+        <h1 className="Banner-description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+
+      <div className="Banner-fadeBottom"></div>
+    </header>
+  );
+};
